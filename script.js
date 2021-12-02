f2dba363b79450a3cf1ab6ae9034016e00fa3177
agres <ale.agresta46@gmail.com>
2021-12-02 15:44:13 +0100

    finished

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const userName = prompt('Inserisci il tuo nome');
+const userLastname = prompt('Inserisci il tuo cognome');
+const color = prompt('Colore preferito')
+
+let a = userName; // number
+let b = userLastname;
+let c = color
+
+let test1 = a + b + c;
+
+document.writeln(test1);
