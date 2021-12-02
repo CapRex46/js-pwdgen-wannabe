4fe89f6708e659fdf6bc5ff26e185afe3750a293
agres <ale.agresta46@gmail.com>
2021-12-02 15:45:58 +0100

    added comments

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-
+// inserisco i dati
 const userName = prompt('Inserisci il tuo nome');
 const userLastname = prompt('Inserisci il tuo cognome');
 const color = prompt('Colore preferito')
-
+// nomino i dati
 let a = userName; // number
 let b = userLastname;
 let c = color
-
+// sommo i dati
 let test1 = a + b + c;
-
+// risultato
 document.writeln(test1);
